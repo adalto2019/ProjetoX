dcac5ba0d32eb27fd6d7d3018e7e560d231e9b49
AdaltoJr <47403579+adalto2019@users.noreply.github.com>
2023-11-09 23:36:56 -0300

    Subindo docs..

---
+++ js.js
@@ -0,0 +1,22 @@
+ 
+//Funções modal Menu
+var m = document.getElementById('ModalMenu')
+
+function abrirModal(){
+    m.style.display = 'block'
+}
+
+function fecharModal(){
+    m.style.display = 'none'
+}
+
+//Funções Modal Anúncios
+let anuncio = document.getElementById('ModalAnuncio')
+ 
+function ModalAnuncio(){
+    anuncio.style.display = 'block'
+}
+
+function fecharModalAnuncio(){
+    anuncio.style.display = 'none'
+}
